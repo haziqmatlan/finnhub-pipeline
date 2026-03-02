66fecaa8f1f6dd44aa2bf20fad23a8aa3612521c
haziq_matlan <haziq.matlan@gmail.com>
2026-03-02 22:33:09 +0800

    modified:   pipelines/data_pipeline/__init__.py

---
+++ pipelines/data_pipeline/__init__.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+from pipelines.core.constant import environments, spaces
+from pipelines.core.util.configuration_util import SubparserBuilder
+
+
+@SubparserBuilder
+def build_subparsers(subparsers) -> list[ArgumentParser]:
+    """
+    Build subparsers for the tasks in this module
+
+    :param subparsers: A subparsers object from `argparse.ArgumentParser.add_subparsers()`
+    :return: List of ArgumentParser
+    """
+
+    parsers: list[ArgumentParser] = []
+    parser: ArgumentParser
+
+    task = 'pipelines.data_pipeline.raw'
+    parser = subparsers.add_parser(task)
+    parser.set_defaults(command=task)
+    parser.add_argument('-b', '--bucket', help='S3 bucket')
+    parser.add_argument('-e', '--env', choices=environments, required=True, help='Environment')
+    parser.add_argument('-s', '--space', choices=spaces, required=True, help='Space')
+    parser.add_argument('--config', help='Configuration file')
+    parsers.append(parser)
+
+    return parsers
